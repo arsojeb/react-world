5028deff50e79a1a67cff702eff3e09d150c0bfc
arsojeb <abdurrahaman7042@gmai.com>
2025-09-22 18:52:27 +0600

    third

--- src/assets/country/country.jsx
+++ src/assets/country/country.jsx
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./countryOne.css"
 
-const country = ({country}) => {  
-    console.log(country.population.population)
+const country = ({country, handelVisitedCountries}) => { 
+    const [visited, setVisited] = useState(false);
+    //console.log(country.population.population)
+    //console.log(handelVisitedCountries)
+    const handelVisited = () =>{
+
+        //basic
+
+        //if(visited){
+            //setVisited(false)
+        //}
+        //else{
+            //setVisited(true)
+        //}
+
+        //mid
+        
+        //setVisited(visited? false : true);
+
+        //final
+
+        setVisited(!visited);
+        handelVisitedCountries(country)
+    }
+
     return (
-        <div className='country'>
+        <div className= {`country ${visited && "country-visited"}`} >
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h3>Name:{country.name.common}  </h3>
             <h4>Population: {country.population.population}</h4>
             <h4>Area: {country.area.area} {country.area.area > 300000 ? "Big Country" : "Small Country"} </h4>
-            <button>Not Visited</button>
+            <button onClick={handelVisited}>
+                {visited ? "Visited" : "Not Visited"}
+            </button>
         </div>
     );
 };
--- src/countrys.jsx
+++ src/countrys.jsx
@@ -1,11 +1,17 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import Country from './assets/country/country';  
 import country from './assets/country/country';
 import "./countryTwo.css"
 
 const Countrys = ({ countryPromis }) => {
+    const [visitedCountries, setvisitedCountries] = useState([]);
+
+    const handelVisitedCountries = (country) => {
+        console.log('handel visited countries',country)
+    }
+
     const countrisData = use(countryPromis);       
-    const countries = countrisData.countries || [];
+    const countries = countrisData.countries;
 
     console.log(countries);
 
@@ -13,8 +19,9 @@ const Countrys = ({ countryPromis }) => {
         <div>
             
             <h1>In The Country: {countries.length}</h1>
+            <h3>Total Country Visited:</h3>
             <div className='countries'>{countries.map(country => (
-                <Country key={country.cca3.cca3} country={country}></Country>
+                <Country key={country.cca3.cca3} country={country} handelVisitedCountries ={handelVisitedCountries}></Country>
             ))}</div>
             
         </div>
